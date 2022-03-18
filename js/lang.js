f385623f2f0fc700acfcafcf50f6ebd35d0eca07
buggy-des <101577453+buggy-des@users.noreply.github.com>
2022-03-18 15:47:41 +0200

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,112 @@
+const langArr = {
+    "unit" : {
+        "ua": "ілюстратор Марія Омеліна",
+        "eng": "illustrator omelina.art site",
+        "deu": "Illustratorin Maria Omelina",
+    },
+    "socialhome-social": {
+        "ua": "Додому",
+        "eng": "Home",
+        "deu": "Zuhause",
+    },
+    "socialabout-social": {
+        "ua": "Про мене",
+        "eng": "About me",
+        "deu": "Über mich",
+    },
+    "socialskills-social": {
+        "ua": "Скіли",
+        "eng": "Skills",
+        "deu": "Fähigkeiten",
+    },
+    "socialportfolio-social" : {
+        "ua": "Портфоліо",
+        "eng": "Portfolio",
+        "deu": "Portfolio",
+    },
+    "socialcontacts-social": {
+        "ua": "Контакти",
+        "eng": "Contacts",
+        "deu": "Kontakte",
+    },
+    "name": {
+        "ua": "Марія Омеліна",
+        "eng": "Mariia Omelina",
+        "deu": "Mariia Omelina",
+    },
+    "spec": {
+        "ua": "Ілюстратор/Художник, 28 років, Київ",
+        "eng": "Illustrator/Painter, 28 years old, Kyiv",
+        "deu": "Illustrator/Maler, 28 Jahre alt, Kiew",
+    },
+    "am": {
+        "ua": "Про мене",
+        "eng": "About me",
+        "deu": "Über mich",
+    },
+    "am1": {
+        "ua": "Вітаю, я Марія - ілюстратор з Києва.",
+        "eng": "Hi, I'm Mariia - an illustrator from Kyiv.",
+        "deu": "Hallo, ich bin Mariia - eine Illustratorin aus Kiew.",
+    },
+    "am2": {
+        "ua": "Цікавлюся мистецтвом, малюванням та всим, що з цим пов'язано",
+        "eng": "Interested in art, drawing and everything connected with it.",
+        "deu": "Ich interessiere mich für Kunst, Zeichnen und alles was damit zusammenhängt.",
+    },
+    "am3": {
+        "ua": "Навчалась в Київській дитячій школі мистецтв №7.",
+        "eng": "Studied drawing at Kyiv children's art school #7.",
+        "deu": "Ich habe Zeichnen an der Kiewer Kinderkunstschule Nr. 7 studiert.",
+    },
+    "am4": {
+        "ua": "Я малюю ескізи, стікерпаки, портрети, ілюстрації.",
+        "eng": "I'm creating sketches, sticker packs, portraits, illustrations.",
+        "deu": "Ich erstelle Skizzen, Stickerpakete, Portraits, Illustrationen.",
+    },
+    "am5": {
+        "ua": "Готова реалізовувати цікаві проекти з чудовими людьми.",
+        "eng": "Ready to implement excellent projects with wonderful people.",
+        "deu": "Bereit, exzellente Projekte mit wunderbaren Menschen umzusetzen.",
+    },
+    "skdesc": {
+        "ua": "Скіли",
+        "eng": "Skills",
+        "deu": "Fähigkeiten",
+    },
+    "sk": {
+        "ua": "Працюю в наступних програмах",
+        "eng": "I work in such programs as",
+        "deu": "Ich verwende folgende Programme",
+    },
+    "contacts": {
+        "ua": "Контакти",
+        "eng": "Contacts",
+        "deu": "Kontakte",
+    },
+    "contacts-text": {
+        "ua": "Хочете дізнатися більше чи просто поспілкуватися?",
+        "eng": "Want to know more or just chat?",
+        "deu": "Möchten Sie mehr wissen oder einfach nur chatten?",
+    },
+    "contacts-text1": {
+        "ua": "Будь ласка!",
+        "eng": "You are welcome!",
+        "deu": "Gern geschehen!",
+    },
+    "message": {
+        "ua": "Відправ повідомлення",
+        "eng": "Send message",
+        "deu": "Eine Nachricht schicken",
+    },
+    "like": {
+        "ua": "Підписуйся в",
+        "eng": "Like me on",
+        "deu": "Folge mir",
+    },
+    "copy": {
+        "ua": "&#169; 2022 Всі права захищені.",
+        "eng": "&#169; 2022 All rights reserved.",
+        "deu": "&#169; 2022 Alle Rechte vorbehalten.",
+    },
+}
